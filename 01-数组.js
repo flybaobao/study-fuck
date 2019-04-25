41660af09dd9c9f562bdaaab7457c99954ee8085
flybaobao <songd@8518.com>
2019-04-25 20:08:05 +0800

    数组几种方法遍历

--- 01-数组.js
+++ 01-数组.js
@@ -14,10 +14,16 @@ arr.forEach(function (item) {
 
 for (let key in arr) { // key 是下标
     console.log(key);
-    console.log(typeof  key); //字符串   会遍历数组外的属性也会遍历出来
+    console.log(typeof  key); //字符串   会遍历数组外的私有// 属性也会遍历出来
 }
 
 for (let val of arr) {
     console.log(val);// 支持return 并且是值 of 数组（不能遍历对象）
 }
 
+let obj = {school: 'zfa',age: 12};
+Object.keys(obj);
+console.log(Object.keys(obj));
+for(let val of Object.keys(obj)){
+    console.log(val);
+};
